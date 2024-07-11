dc6367b152fc381370250ee95ec353eb5438901b
ljs3536 <wlstjr320@naver.com>
2024-07-11 23:42:49 +0900

    layout

--- mall/src/pages/AboutPage.js
+++ mall/src/pages/AboutPage.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
 function AboutPage(props) {
   return (
-    <div className="{'text-3xl}">
-      <div className="{'flex'}">
-        <Link to={"/"}>About</Link>
-      </div>
-      <div>About Page</div>
-    </div>
+    <BasicLayout>
+      <div className={"text-3xl"}>About Page</div>
+    </BasicLayout>
   );
 }
 
--- mall/src/pages/MainPage.js
+++ mall/src/pages/MainPage.js
@@ -1,14 +1,11 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
 
 function MainPage() {
   return (
-    <div className="{'text-3xl}">
-      <div className="{'flex'}">
-        <Link to={"/about"}>About</Link>
-      </div>
-      <div>Main Page</div>
-    </div>
+    <BasicLayout>
+      <div className={"text-3xl"}>Main Page</div>
+    </BasicLayout>
   );
 }
 
